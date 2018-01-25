d61cae7dacc5204be2a35cdccfa9be5a7af91fb5
srinivas <srinivas.c1992@gmailc.com>
2018-01-25 20:13:24 +0530

    Initial Project creation

---
+++ src/main/java/com/example/ClassTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class ClassTest {
+	
+	public void callingMethod(){
+	}
+	
+	public static void main(String[] args) {
+		NewClass newClass = new NewClass(1, 2);
+		newClass.print();
+	}
+	
+}
---
+++ src/main/java/com/example/NewClass.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class NewClass {
+	int a;
+	int b;
+	int c;
+	
+	public NewClass(int f, int g) {
+		this.a = f;
+		this.b = g;
+	}
+	
+	public void print(){
+		int b =10;
+		System.out.println(this.a);
+		System.out.println(b);
+	}
+}
